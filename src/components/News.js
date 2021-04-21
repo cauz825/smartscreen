86e96fa0e59e6da8f56edf3e4701872b65ba8ac4
MCauz <michaelhcausey@gmail.com>
2021-04-21 16:00:08 -0700

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function News() {
+    return(
+        <div>
+            News Component
+        </div>
+    )
+}
+
+export default News
---
+++ src/components/Weather.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Weather() {
+    return(
+        <div>
+            Weather Component
+        </div>
+    )
+}
+
+export default Weather
