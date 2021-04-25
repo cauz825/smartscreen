dbaa2f558ae9d1e78db97c25645902cc70a836f0
MCauz <michaelhcausey@gmail.com>
2021-04-25 14:18:21 -0700

    moment installed

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+const moment = require('moment')
 
 function Time() {
+
+    const today = moment();
+    const [currentDate, setCurrentDate] = useState('')
+    const [currentTime, setCurrentTime] = useState('')
+    const [countdown, setCountdown] = useState('')
+
+    function displayDate() {
+        setCurrentDate(today.format("dddd[,] MMMM Mo YYYY"))
+    }
+
+    function displayTime() {
+        setCurrentTime(today.format("h:mmA"))
+    }
+
+    function daysLeft() {
+        const costaRicaDate = moment("2021-06-08")
+        setCountdown(costaRicaDate.diff(today, "days"))
+    }
+
+    useEffect(() => {
+       displayDate()
+       displayTime()
+       daysLeft()
+    })
+
     return(
         <div>
-
+           Today is {currentDate}<br></br>
+           It is {currentTime}<br></br>
+           Costa Rica is {countdown} days!
         </div>
     )
 }
