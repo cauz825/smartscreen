464072326c2545896fa6340e3958b1808034c5a1
MCauz <michaelhcausey@gmail.com>
2021-04-25 13:21:58 -0700

    added time component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Calendar from './components/Calendar'
 import News from './components/News'
 import Weather from './components/Weather'
+import Time from './components/Time'
 
 function App() {
   return(
     <div>
       Smartscreen App<br></br><br></br>
+      <Time /><br></br>
       <Calendar />
       <News />
       <Weather />
